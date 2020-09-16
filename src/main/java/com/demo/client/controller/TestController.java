02993a44108f8836bd56c4f441aed05c3754ca3f
JaminHuang <huanghzm@126.com>
2020-09-16 14:54:57 +0800

    删除引用

--- src/main/java/com/demo/client/controller/TestController.java
+++ src/main/java/com/demo/client/controller/TestController.java
@@ -7,7 +7,6 @@ import com.demo.sdk.controller.BaseController;
 import com.demo.sdk.response.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
